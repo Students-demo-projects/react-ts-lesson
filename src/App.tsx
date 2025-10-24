40858f739c53c756fbb63e74a189626144eb1b72
Otabek1990 <fozilovotabek424@gmail.com>
2025-10-24 16:32:38 +0500

    login page yaratildi navga stil berildi

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { lazy} from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header.tsx";
 
-const Home = lazy(() => import("./pages/home/index.tsx"));
-
+const Home = lazy(() => import("./pages/home"));
+const Login =lazy(()=>import("./pages/login"))
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         
       </Routes>
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import { NavLink } from "react-router-dom"
 
 
 function Header() {
   return (
-    <nav>
+    <nav className="flex items-center px-16 py-5 bg-blue-500 text-white gap-5">
       <h1>Logo</h1>
+      <NavLink to={"/"}>Home</NavLink>
+      <NavLink to={"/login"}>Login</NavLink>
     </nav>
   )
 }

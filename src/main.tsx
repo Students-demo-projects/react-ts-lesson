2d4f268a4f5971b52deb30b478d80902b5ea72bb
Otabek1990 <fozilovotabek424@gmail.com>
2025-10-27 16:09:50 +0500

    redux installed

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Suspense } from "react";
+import { store } from "./app/store.ts";
+import { Provider } from "react-redux";
+
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Suspense fallback={<h1>loading...</h1>}>
+    <Provider store={store}>
       <App />
+      </Provider>
     </Suspense>
   </BrowserRouter>
 );
